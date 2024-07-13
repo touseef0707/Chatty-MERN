0a1f09b5ddbcef6af866186c008b0714dca99b22
Touseef Ahmed <touseefahmed0707@gmail.com>
2024-07-13 20:36:22 +0400

    online status fix

--- frontend/src/components/messageContainer/TitleBar.jsx
+++ frontend/src/components/messageContainer/TitleBar.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import useConversation from '../../zustand/useConversation'
+import { useSocketContext } from "../../context/SocketContext";
 
 const TitleBar = () => {
-    const { selectedConversation } = useConversation()
+    const {selectedConversation} = useConversation();
+
+    const {onlineUsers} = useSocketContext();
+    const isOnline = onlineUsers.includes(selectedConversation._id);
+
     return (
         <div className="chatHeader flex items-center space-x-5 backdrop-filter backdrop-blur-3xl rounded-2xl bg-opacity-60 px-5 py-2">
             <div className="relative inline-block w-11">
                 <img src={selectedConversation.profilePicture} alt="profile" className="w-full h-full" />
-                <span className="absolute bottom-0 right-0 block w-3 h-3 bg-green-500 rounded-full border-2 border-white"></span>
+                {isOnline && <span className="absolute bottom-0 right-0 block w-3 h-3 bg-green-500 rounded-full border-2 border-white"></span>}
             </div>
             <div className="name flex">
                 <h1 className="text-white font-bold text-xl flex justify-start">{selectedConversation.fullName}</h1>
